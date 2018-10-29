346b0fcc9ac0c309d1f00adad9401d96f6718605
Jorge hernandez <jorge_hdezdev@hotmail.com>
2018-10-29 07:39:21 -0800

    subiendo css pro

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,20 +1,20 @@
 @include('header')
-	  <link rel="stylesheet" href="css/pro.css">
-	  <div>
+	  <link rel="stylesheet" href="{{url  ('/css/pro.css)}}">
+<div class="muestraimagenes">
 	<h1>productos</h1>
 
 	<ul class="galeria">
-		<li><a href=""><img src="{{('images/imagen1.png')}}"> </a></li>
-		<li><a href=""><img src="{{('images/imagen2.png')}}"> </a></li>
-		<li><a href=""><img src="{{('images/imagen3.png')}}"> </a></li>
-		<li><a href=""><img src="{{('images/imagen4.png')}}"> </a></li>
+		<li><a href=""><img src="{{url  ('/images/imagen1.png')}}"> </a></li>
+		<li><a href=""><img src="{{url  ('/images/imagen2.png')}}"> </a></li>
+		<li><a href=""><img src="{{url  ('/images/imagen3.png')}}"> </a></li>
+		<li><a href=""><img src="{{url  ('/images/imagen4.png')}}"> </a></li>
 	</ul>
 
 	<div class="modal">
 		<h3>Paisaje 1</h3>
 		<div class="imagen">
 			<a href="">&#60;</a>
-			<a href=""><img src="{{('images/imagen3.png')}}"></a>
+			<a href=""><img src="{{ url('images/imagen3.png')}}"></a>
 			<a href="">></a>
 		</div>
 		<a href="">X</a>
